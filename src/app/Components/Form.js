1d7b1bef2cc4ce6e0754f4922600060b1d426c3f
PahanZ <zagvzdin.pakhan@rambler.ru>
2018-04-10 14:43:16 +0300

    form control

--- src/app/Components/Form.js
+++ src/app/Components/Form.js
@@ -6,51 +6,82 @@ import {Form, Icon, Input, DatePicker, Col, Button} from 'antd';
 const FormItem = Form.Item;
 const InputGroup = Input.Group;
 const {TextArea} = Input;
-
-export default class HorizontalForm extends React.Component {
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(e);
-  }
-  render() {
-    return (
-      <main>
-        <Form layout="horizontal" onSubmit={this.handleSubmit}>
-          <InputGroup size="large">
-            <Col span={12}>
-              <FormItem label="Number">
-                <Input size="large" addonAfter={<Icon type="setting"/>} defaultValue="INV "/>
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Invoice Date">
-                <DatePicker size="large"/>
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Supply Date">
-                <DatePicker size="large"/>
-              </FormItem>
-            </Col>
-            <Col span={24}>
-              <FormItem label="Comment">
-                <TextArea rows={24}/>
-              </FormItem>
-            </Col>
-            <Col span={24}>
-              <div className="submit">
-                <Button type="primary">Save
-                  {/* <Link to="/">Save</Link> */}
-                </Button>
-              </div>
-            </Col>
-          </InputGroup>
-        </Form>
-      </main>
-    );
-  }
-}
+const HorizontalForm = props => {
+  const {number, invoiceDate, supplyDate, comment} = props.inputsAtributes;
+  return (
+    <main>
+      <Form
+        layout="horizontal" // onSubmit={this.handleSubmit}
+        >
+        <InputGroup size="large">
+          <Col span={12}>
+            <FormItem label="Number">
+              <Input
+                name={number.name}
+                size="large"
+                addonAfter={<Icon type="setting"/>}
+                value={number.value}
+                onChange={function (event) {
+                  props.onChange(number.name, event);
+                }}
+                />
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem label="Invoice Date">
+              <DatePicker
+                name={invoiceDate.name}
+                size="large"
+                value={invoiceDate.value}
+                format={'Do MMM YY'}
+                onChange={function (event) {
+                  props.onChange(invoiceDate.name, event);
+                }}
+                />
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem label="Supply Date">
+              <DatePicker
+                name={supplyDate.name}
+                size="large"
+                value={supplyDate.value}
+                format={'Do MMM YY'}
+                onChange={function (event) {
+                  props.onChange(supplyDate.name, event);
+                }}
+                />
+            </FormItem>
+          </Col>
+          <Col span={24}>
+            <FormItem label="Comment">
+              <TextArea
+                name={comment.name}
+                rows={24}
+                value={comment.value}
+                onChange={function (event) {
+                  props.onChange(comment.name, event);
+                }}
+                />
+            </FormItem>
+          </Col>
+          <Col span={24}>
+            <div className="submit">
+              <Button type="primary" htmlType="submit">Save
+                {/* <Link to="/">Save</Link> */}
+              </Button>
+            </div>
+          </Col>
+        </InputGroup>
+      </Form>
+    </main>
+  );
+};
+// }
 
 HorizontalForm.propTypes = {
-  form: PropTypes.object
+  inputsAtributes: PropTypes.object,
+  onChange: PropTypes.func
 };
+
+export default HorizontalForm;
--- src/app/Pages/Page2.js
+++ src/app/Pages/Page2.js
@@ -1,4 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 import Form from '../Components/Form';
 
-export default () => <Form/>;
+export default class Page2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: {name: 'number', value: 'INV '},
+      invoiceDate: {name: 'invoiceDate', value: moment()},
+      supplyDate: {name: 'supplyDate', value: moment()},
+      comment: {name: 'comment', value: ''}
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(objName, e) {
+    const newValue = (e.target) ? e.target.value : e;
+    this.setState({[objName]: {name: objName, value: newValue}});
+  }
+  render() {
+    return (
+      <Form inputsAtributes={this.state} onChange={this.handleChange}/>
+    );
+  }
+}
